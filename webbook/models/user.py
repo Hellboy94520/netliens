ba646f4ae5cff16fb1de347d1601f2d07d5e66d1
Hellboy94520 <alexandre.delahaye.94@gmail.com>
2022-02-12 17:31:05 +0100

    Fix user creation issue

--- webbook/models/user.py
+++ webbook/models/user.py
@@ -12,17 +12,51 @@ class TokenGenerator(PasswordResetTokenGenerator):
 account_activation_token = TokenGenerator()
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser ## A new class is imported. ##
+from django.contrib.auth.models import AbstractUser, BaseUserManager ## A new class is imported. ##
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinValueValidator
 
 # -----------------------------
 # Model
 # -----------------------------
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [ 'last_name', 'first_name']
+
+    objects = CustomUserManager()
 
     is_vip = models.BooleanField(
         default=False,
@@ -90,6 +124,9 @@ class User(AbstractUser):
         default=0,
         verbose_name=_("NL Level 7"),
         help_text=_("NL Level 10 Purchase Quantity"))
+
+    def __str__(self):
+        return self.email
 
 User._meta.get_field('email')._unique = True
 for fieldname in ['email', 'last_name', 'first_name']:
